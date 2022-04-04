7e4aafbbd5f23e0063c87ceb27ef33a68cabbe46
Konstantin Kocha <konstantin.kocha@teligent.ru>
2022-04-04 11:26:40 +0300

    update dependencies

--- src/preset.js
+++ src/preset.js
@@ -1,3 +1,3 @@
 export function managerEntries(entry = []) {
-  return [...entry, require.resolve("./register")]; //👈 addon implementation
+  return [...entry, require.resolve("./register")]; // 👈 addon implementation
 }
